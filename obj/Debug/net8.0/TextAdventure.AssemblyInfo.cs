ad13aaba17a9be295c1946c55a6ae9c98a64d5c5
bcushenberry <cushenberry@gmail.com>
2024-03-22 16:42:12 +0900

    Initial commit

--- obj/Debug/net8.0/TextAdventure.AssemblyInfo.cs
+++ obj/Debug/net8.0/TextAdventure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextAdventure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8a21be9bf771075d94169aeaa0821f22d4f1799")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextAdventure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextAdventure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
